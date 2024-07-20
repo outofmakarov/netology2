d3193a6170d10b83b7c01cd7c718da61ef7bb8e0
outofmakarov <6360058@mail.ru>
2024-07-20 17:20:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.ObjectStreamClass;
+
+public class Main {
+    private static void main(String[] args) {
+        double weightCat = 4.5;
+        int ageCat = 4;
+        String nameCat = "Леви";
+
+        System.out.println(ageCat);
+        System.out.println(weightCat);
+
+        }
+}
